e811af69723932d478726f0db80ffe1d968873e7
Danylo Skepskyi <kol230305@gmail.com>
2024-11-28 21:40:08 +0100

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -17,3 +17,6 @@ class Car(models.Model):
         Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    class Meta:
+        ordering = ("model",)
