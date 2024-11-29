e6b1f085578c1cb6aaf0d9688a0b1a003bcb0db2
Danylo Skepskyi <kol230305@gmail.com>
2024-11-29 16:27:07 +0100

    add empty lines

--- taxi/models.py
+++ taxi/models.py
@@ -9,6 +9,9 @@ class Manufacturer(models.Model):
     class Meta:
         ordering = ("name",)
 
+    def __str__(self):
+        return self.name
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
